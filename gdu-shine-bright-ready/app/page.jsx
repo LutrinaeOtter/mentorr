fabd721099dc69e9d13912afceeb7e378ece6814
LutrinaeOtter <maiphuc206@gmail.com>
2025-10-25 14:50:49 +0700

    Add files via upload

---
+++ gdu-shine-bright-ready/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>✨ GDU Shine Bright ✨</h1>
+      <p>Website của bạn đã sẵn sàng để xuất bản!</p>
+    </main>
+  );
+}
